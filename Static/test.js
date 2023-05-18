1a1b3fe184c8003bfb7a7a5781e6472208aa0cbd
Joaquin Orellana <joaquinosotelo@gmail.com>
2023-05-18 15:50:34 -0400

    agregamos la api correctamente

--- Static/test.js
+++ Static/test.js
@@ -24,7 +24,50 @@
       $("#prue").show();
     });
     
-    $("button").click(function(){
+    $("#envio").click(function(){
       alert("Se estan enviando los datos. ");
     });
   });
+  
+  $(document).ready(function(){
+    $("#boton").click(function convertir(){
+      /*
+        var myHeaders = new Headers();
+        myHeaders.append("apikey", "aLrg0fHteOAuBGetI72VtEgn7QEfBbgW");
+    
+        var requestOptions = {
+          method: 'GET',
+          redirect: 'follow',
+          headers: myHeaders
+        };
+    
+        fetch("https://api.apilayer.com/fixer/convert?to="+hacia+"&from="+desde+"&amount="+cantidad+"", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+        var resultado = result
+        
+      });*/
+      var ha = document.getElementById("ha");
+      var de = document.getElementById("de");
+      var ca = document.getElementById("ca");
+      
+      var cantidad = ca.value;
+      var desde = de.value;
+      var hacia = ha.value;
+
+      var settings = {
+          "url": "https://api.apilayer.com/fixer/convert?from="+desde+"&amount="+cantidad+"&apikey=aLrg0fHteOAuBGetI72VtEgn7QEfBbgW&to="+hacia+"",
+            "method": "GET",
+            "timeout": 0,
+          };
+          
+        $.ajax(settings).done(function (response) {
+          console.log(response);
+          var numero_con_decimales = response.result;
+          var respuesta = Math.floor(numero_con_decimales);
+          $("#resultado").append("La cantidad de " +cantidad+ " en " +desde+ " hacia " +hacia+ " es de " +respuesta+ "");
+      });
+  });
+});
+
